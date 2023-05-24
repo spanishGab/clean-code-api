d29416b013d5d7342345324da584571976729c28
Gabriel Espanhol Santos <gabrielmail09@gmail.com>
2023-05-24 09:09:09 -0300

    feat: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/sugnup.ts
+++ src/presentation/controllers/sugnup.ts
@@ -1,7 +1,8 @@
 export class SignUpController {
   public handle (hettpRequest: any): any {
     return {
-      statusCode: 400
+      statusCode: 400,
+      body: new Error('Missing param: name')
     }
   }
 }
